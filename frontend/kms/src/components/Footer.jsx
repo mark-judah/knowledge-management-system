8025a05dd6c5529ba2037e0fb0ec53620d218881
mark-jk <markjudah8@gmail.com>
2024-11-12 00:31:03 +0300

    removed tailwind breakpoints color guide from footer

--- frontend/kms/src/components/Footer.jsx
+++ frontend/kms/src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer class="w-full bg-black sm:bg-black md:bg-red-500 lg:bg-green-500 xl:bg-purple-500 2xl:bg-yellow-500 p-8">
+        // <footer class="w-full bg-black sm:bg-black md:bg-red-500 lg:bg-green-500 xl:bg-purple-500 2xl:bg-yellow-500 p-8">
+        <footer class="w-full bg-black p-8 mt-8">
             <p class="block mb-4 text-sm text-center text-slate-500 md:mb-0 border-t border-black mt-4 pt-4">
                 Copyright © 2024&nbsp; Ipsum
             </p>
