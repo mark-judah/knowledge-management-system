3897ef9d97ab812308b8c55f097a7e6faf1929e4
mark-jk <markjudah8@gmail.com>
2024-11-12 16:17:20 +0300

    added vercel.json

--- frontend/kms/mock_server.js
+++ frontend/kms/mock_server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+module.exports=server
