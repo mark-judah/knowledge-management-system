0241c06308251f500b794f027f9d86ea626db10b
mark-jk <markjudah8@gmail.com>
2024-11-12 16:33:12 +0300

    fixed db.json import path

--- frontend/kms/api/mock_server.js
+++ frontend/kms/api/mock_server.js
@@ -1,10 +1,16 @@
 const jsonServer = require('json-server');
+const path = require('path')
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router);
 
 server.listen(port);
